30f1dc121cdc8fa51773015c14017276f2bf9191
jwc20 <choi.jae100@gmail.com>
2025-07-14 13:41:03 +0900

    todo: create rooms

--- main.py
+++ main.py
@@ -1,61 +1,14 @@
-"""
-this is 3.py but using encode/broadcaster
-"""
-
 import anyio
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
-from fastapi.responses import HTMLResponse
+from fastapi.responses import FileResponse
 
 from broadcaster import Broadcast
 
 broadcast = Broadcast("redis://localhost:6379")
 
-app = FastAPI(
-    on_startup=[broadcast.connect],
-    on_shutdown=[broadcast.disconnect]
-)
+app = FastAPI(on_startup=[broadcast.connect], on_shutdown=[broadcast.disconnect])
 
 
-html = """
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Chat</title>
-    </head>
-    <body>
-        <h1>WebSocket Chat</h1>
-        <h2>Your ID: <span id="ws-id"></span></h2>
-        <form action="" onsubmit="sendMessage(event)">
-            <input type="text" id="messageText" autocomplete="off"/>
-            <button>Send</button>
-        </form>
-        <ul id='messages'>
-        </ul>
-        <script>
-            var client_id = Date.now()
-            document.querySelector("#ws-id").textContent = client_id;
-            var ws = new WebSocket(`ws://localhost:8000/ws/${client_id}`);
-            ws.onmessage = function(event) {
-                var messages = document.getElementById('messages')
-                var message = document.createElement('li')
-                var content = document.createTextNode(event.data)
-                message.appendChild(content)
-                messages.appendChild(message)
-            };
-            function sendMessage(event) {
-                var input = document.getElementById("messageText")
-                ws.send(input.value)
-                input.value = ''
-                event.preventDefault()
-            }
-        </script>
-    </body>
-</html>
-"""
-
-
-
-# singleton class
 class ConnectionManager:
     def __init__(self) -> None:
         self.active_connections: list[WebSocket] = []
@@ -81,22 +34,19 @@ class ConnectionManager:
 manager = ConnectionManager()
 
 
-
 @app.get("/")
 async def get():
-    return HTMLResponse(html)
+    return FileResponse("templates/index.html")
 
 
 @app.websocket("/ws/{client_id}")
 async def websocket_endpoint(websocket: WebSocket, client_id: int):
     await manager.connect(websocket)
 
-    
     async def redis_listener():
         async with broadcast.subscribe(channel="chat") as subscriber:
             async for event in subscriber:
                 await manager.broadcast_to_local_connections(event.message)
-
 
     async with anyio.create_task_group() as task_group:
         task_group.start_soon(redis_listener)
@@ -109,5 +59,3 @@ async def websocket_endpoint(websocket: WebSocket, client_id: int):
         except WebSocketDisconnect:
             manager.disconnect(websocket)
             await manager.broadcast(f"Client #{client_id} left the chat")
-
-
